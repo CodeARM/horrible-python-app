b158eecee987180e303674348b660d64e0f5953d
CodeARM <20066673+CodeARM@users.noreply.github.com>
2026-05-18 08:51:04 -0400

    adding files

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Waiting for postgres..."
+
+while ! nc -z db 5432; do
+  sleep 1
+done
+
+echo "Postgres started"
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+gunicorn config.wsgi:application \
+    --bind 0.0.0.0:8000
